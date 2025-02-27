508fec1bbfbb3b67ff5ba8ecd33c5b7baf048b42
sung0615 <kk1hhh1fff1@naver.com>
2025-02-27 11:59:47 +0900

    스프링

---
+++ tamsa/src/main/java/com/aa11aa/demo/TamsaApplication.java
@@ -0,0 +1,13 @@
+package com.aa11aa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamsaApplication.class, args);
+	}
+
+}
---
+++ tamsa/src/test/java/com/aa11aa/demo/TamsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa11aa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
